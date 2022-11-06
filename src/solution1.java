de6439a0ebc4dd8b6373be056ccc0dcc2a942430
anuLoveCoading <anusrivas07@gmail.com>
2022-11-06 12:46:50 +0530

    git commit -m "Array2Solution"

--- src/solution1.java
+++ src/solution1.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class solution1 {
+public class solution1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome Array2Solution;");
+    }
 }

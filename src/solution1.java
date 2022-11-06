413d75d0e7cdd8f68cca9c75796b557337bd29a8
anuLoveCoading <anusrivas07@gmail.com>
2022-11-06 14:06:31 +0530

    git commit -m "Array2Solution"

--- src/solution1.java
+++ src/solution1.java
@@ -1,5 +1,45 @@
 public class solution1 {
     public static void main(String[] args) {
-        System.out.println("Welcome Array2Solution;");
+//        Q.1 (MCQ)
+//        int[] arr = {1, 2, 1, 3, 6, 1, 4, 1, 1, 9, 1};
+//        int count = 0;
+//        for (int j : arr) {
+//            if (j == 1) {
+//                count++;
+//            }
+//        }
+//        System.out.print(count);
+
+//        Q.2 (MCQ);
+//        double[] myList = {1, 5, 5, 5, 5, 1};
+//        double max = myList[0];
+//        int index = 0;
+//        for(int i = 1; i < myList.length; i++) {
+//            if(myList[i] > max) {
+//                max = myList[i];
+//                index = i;
+//            }
+//        }
+//        System.out.println(index);
+
+//        Q.3 (MCQ);
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+//        int left = 0;
+//        int right = arr.length - 1;
+//        while(left < right) {
+//            int temp = arr[left];
+//            arr[left] = arr[right];
+//            arr[right] = temp;
+//            left++;
+//            right--;
+//        }
+//        for (int j : arr) {
+//            System.out.print(j + " ");
+//        }
+
+//        Q.4 (MCQ);
+//        int[] arr = {0, 2, 4, 1, 3};
+//        for(int i = 0; i < arr.length; i++)
+//            arr[i] = arr[(arr[i] + 3) % arr.length];
     }
 }
